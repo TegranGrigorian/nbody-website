cfad4bfa14fefbc5fab5f93e016b6fb282ce26ba
TegranGrigorian <tegrgrigor@gmail.com>
2025-11-17 20:31:23 -0500

    Install page finished with table of content !! :)

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = flask.Flask(__name__)
 def homepage():
     return flask.render_template('homepage.html')
 
+@app.route('/install')
+def install_page():
+    return flask.render_template('install.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
